56f8aae288497e58977ec14322fb5136f44eb729
Ibrahim Swaiss <Iaswaiss@outlook.com>
2018-11-03 03:06:12 +0200

    add money controller

---
+++ aspnet-core/src/MoneyTracker/MoneyTracker/Controllers/MoneyController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MoneyTracker.Controllers {
+    [Produces("application/json")]
+    [Route("api/Money")]
+    public class MoneyController : Controller {
+        [HttpGet]
+        public string Index(string name) {
+            return $"Welcome {name} to my money application!";
+        }
+    }
+}
