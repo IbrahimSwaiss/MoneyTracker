e73070cfce3d15eb349985bb414bf0cdc4e2455f
Ibrahim Swaiss <Iaswaiss@outlook.com>
2018-11-03 03:00:18 +0200

    add nSwag

---
+++ aspnet-core/src/MoneyTracker/MoneyTracker/Controllers/HomeController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MoneyTracker.Controllers {
+    public class HomeController : Controller {
+        public ActionResult Index() {
+            return Redirect("/swagger");
+        }
+    }
+}
